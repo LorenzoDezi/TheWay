ec177e1cde5d1d49abcadbc39d7b73ecf618bd5e
LorenzoDezi <lorenzo.dezi@studenti.unicam.it>
2016-11-08 16:27:23 +0100

    Activity principali fatte

--- app/src/main/java/com/unicam/dezio/theway/MainActivity.java
+++ app/src/main/java/com/unicam/dezio/theway/MainActivity.java
@@ -1,14 +1,10 @@
 package com.unicam.dezio.theway;
 
-import android.graphics.Color;
-import android.graphics.drawable.Drawable;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBarActivity;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.Window;
-import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
